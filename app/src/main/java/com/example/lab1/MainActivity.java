33d163fe40674a84c2bdbbb9470b32337157a20e
Tristan Gates <56215102+tristan-gates@users.noreply.github.com>
2021-01-27 13:54:40 -0600

    Added increment, decrement, and text code

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button inc, dec;
+    TextView txt;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inc = findViewById(R.id.increment);
+        dec = findViewById(R.id.decrement);
+        txt = findViewById(R.id.number);
+
+        inc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                count++;
+                txt.setText(count);
+            }
+        });
+
+        dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count--;
+                txt.setText(count);
+            }
+
+        });
     }
 }
